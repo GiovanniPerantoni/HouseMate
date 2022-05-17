153d5a59192bc281ed2f24f73a3e2a72043b738b
luizmurata <luizjunior.murata@studenti.unitn.it>
2022-05-17 16:35:53 +0200

    Implementato gestione login/registrazione con mongodb e jwt.

--- server.js
+++ server.js
@@ -1,69 +1,50 @@
-const express = require('express')
-const db = require('./database.js')
-var sha256 = require('js-sha256').sha256;
+const dotenv = require('dotenv').config();
+const express = require('express');
+const bodyparser = require('body-parser');
+const sha256 = require('js-sha256').sha256;
+const db = require('./database.js');
 
-const pre = "/api/v1"
-const PORT = 3000;
+const PREFIX = process.env.PREFIX;
+const PORT = process.env.PORT;
 
 const app = express()
-
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
 
 //---------------------API SECTION---------------------\\
-app.post(pre + "/login", (req, res) => {
-    console.log(req.query)      //params sono quelli prima del ? (es: /login/123)
-
-    //TODO controllare che i parametri passati ci siaono tutti quelli necessari
-    let q = req.query;
-    let token = db.login(q.email, sha256(q.pass));
-
-    let status = token == null ? 400 : 200;
-    let body;
-    if(status == 400)
-    {
-        body = {
-            "motivation": "user not present"
-        }
-    }
-    else 
-    {
-        body = {
-            "token": token
-        }
+app.post(PREFIX + "/login", async (req, res) => {
+    
+    const { email, pass } = req.body;
+    if (!(email && pass)) {
+      res.status(400).send({"motivation":"Missing parameters in request."});
+      return;
     }
 
-    res.status(status).send(body);
+    const user = await db.login(email, sha256(pass));
+    if (!user) {
+        res.status(400).send({"motivation":"Invalid credentials."});
+    } else {
+        res.status(200).json(user)
+    }
+
 });
 
 
-app.post(pre + "/singup", (req, res) => {
-    console.log(req.query)      //params sono quelli prima del ? (es: /login/123)
-    
-    //TODO controllare che i parametri passati ci siaono tutti quelli necessari
-    
-    let q = req.query;
-    let token = db.createAccount(q.nome, q.cognome, q.email, sha256(q.pass));
+app.post(PREFIX + "/signup", async (req, res) => {
 
-	//TODO provare mailer con gmail (mandare un token temporaneo di conferma email prima di inserire un nuovo utente)
-
-    let status = token == null ? 400 : 200;
-    let body;
-    if(status == 400)
-    {
-        body = {
-            "motivation": "email already used"
-        }
-    }
-    else 
-    {
-        body = {
-            "token": token
-        }
+    const { first_name, last_name, email, pass } = req.body;
+    if (!(email && pass && first_name && last_name)) {
+        res.status(400).send({"motivation":"Missing parameters in request."});
+        return;
     }
 
-    res.status(status).send(body);
+    const user = await db.createAccount(first_name, last_name, email, sha256(pass));
+    if (!user) {
+        res.status(400).send({"motivation":"Email already used."});
+    } else {
+        res.status(200).json(user)
+    }
 });
 
 //---------------------WEBSITE SECTION---------------------\\
-
-
 app.listen(PORT)
