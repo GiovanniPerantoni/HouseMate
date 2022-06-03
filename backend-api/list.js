1aa3a16fed8397c50366e8b2dee21e15d9299ec0
GiovanniPerantoni <giovanni.perantoni@studenti.unitn.it>
2022-06-03 18:38:10 +0200

    aggiunta documentazione per add e modify

--- backend-api/list.js
+++ backend-api/list.js
@@ -1,7 +1,91 @@
 const list = require("../backend-db/list");
 const com = require("./common");
 
-// /apartment/list/add
+/**
+ * @swagger
+ * /apartment/list/add:
+ *  post:
+ *   summary: Add a new element to the shopping list
+ *   description: 'This method is used to add a **new element** to the shopping list'
+ *   parameters:
+ *   - name: x-access-token
+ *     in: header
+ *     description: Authentication token required for access.
+ *     required: true
+ *     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6I...'
+ *   requestBody:
+ *     content:
+ *      application/json:
+ *       schema:
+ *        required:
+ *        - product
+ *        - date
+ *        properties:
+ *         product:
+ *          type: string
+ *         date:
+ *           type: date
+ *       example:
+ *        product: 'Mozzarella'
+ *        date: '2022-01-01T12:00'
+ *   responses:
+ *    '200':
+ *      description: 'Everything went smoothly.'
+ *    '400':
+ *      description: >-
+ *       This response is sent when the user doesn't reside in an apartment or when the body parameters are of the **wrong type** or if any body parameter is **missing**.
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         required:
+ *         - motivation
+ *         properties:
+ *          motivation:
+ *           type: string
+ *        example:
+ *         motivation: >-
+ *          Couldn't add expense.
+ *    '401':
+ *     description: 'This response is sent if the provided authentication `token` is invalid or expired.'
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        required:
+ *        - motivation
+ *        properties:
+ *         motivation:
+ *          type: string
+ *       example:
+ *        motivation: 'Invalid or expired token.'
+ *    '403':
+ *     description: 'This response is sent if no authentication `token` is provided.'
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        required:
+ *        - motivation
+ *        properties:
+ *         motivation:
+ *          type: string
+ *       example:
+ *        motivation: 'A token is required for authentication.'
+ *    '500':
+ *     description: 'This response is sent if some **unexpected internal error** occurs during execution.'
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        required:
+ *        - motivation
+ *        properties:
+ *         motivation:
+ *          type: string
+ *       example:
+ *        motivation: 'Unexpected error.'
+ */
 async function add(req, res) {
     try {
         const { product, date } = req.body;
@@ -30,7 +114,97 @@ async function add(req, res) {
     }
 }
 
-// /apartment/list/modify
+/**
+ * @swagger
+ * /apartment/list/modify:
+ *  patch:
+ *   summary: Modify an entry of the shopping list
+ *   description: >-
+ *    This method is used to change one of the **entries of the shopping list** which either has to belong to the current user if the user isn't the owner or doesn't have to, if the user is the owner.
+ *   parameters:
+ *   - name: x-access-token
+ *     in: header
+ *     description: Authentication token required for access.
+ *     required: true
+ *     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6I...'
+ *   requestBody:
+ *     content:
+ *      application/json:
+ *       schema:
+ *        required:
+ *        - itemID
+ *        properties:
+ *         itemID:
+ *          type: string
+ *         product:
+ *          type: string
+ *         buyer:
+ *          type: string
+ *         lastBougth:
+ *          type: date
+ *       example:
+ *        itemID: '4e5dcba29...'
+ *        product: 'Pasta'
+ *        Buyer: 'Polnareff'
+ *        date: '2022-01-02T13:45'
+ *   responses:
+ *    '200':
+ *      description: 'Everything went smoothly.'
+ *    '400':
+ *      description: >-
+ *       This response is sent if the expense either doesn't exist or isn't modifiable by the user, or when the body parameters are of the **wrong type** or if any body parameter is **missing**.
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         required:
+ *         - motivation
+ *         properties:
+ *          motivation:
+ *           type: string
+ *        example:
+ *         motivation: >-
+ *          Can't access list element.
+ *    '401':
+ *     description: 'This response is sent if the provided authentication `token` is invalid or expired.'
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        required:
+ *        - motivation
+ *        properties:
+ *         motivation:
+ *          type: string
+ *       example:
+ *        motivation: 'Invalid or expired token.'
+ *    '403':
+ *     description: 'This response is sent if no authentication `token` is provided.'
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        required:
+ *        - motivation
+ *        properties:
+ *         motivation:
+ *          type: string
+ *       example:
+ *        motivation: 'A token is required for authentication.'
+ *    '500':
+ *     description: 'This response is sent if some **unexpected internal error** occurs during execution.'
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        required:
+ *        - motivation
+ *        properties:
+ *         motivation:
+ *          type: string
+ *       example:
+ *        motivation: 'Unexpected error.'
+ */
 async function modify(req, res) {
     try {
         const { itemID, product, buyer, lastBought } = req.body;
