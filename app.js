8af6e0b522e603bb45a3f882fa2b67d506dca06f
giovanni.perantoni@studenti.unitn.it <cazzate.ne>
2022-06-07 21:38:25 +0200

    corse

--- app.js
+++ app.js
@@ -19,7 +19,19 @@ const PREFIX = process.env.PREFIX;
 const app = express()
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use(cors());
+let whitelist = ['http://127.0.0.1:3000/', 'https://ingsoft2-housemate.herokuapp.com/']
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    if(!origin) return callback(null, true);
+    if(whitelist.indexOf(origin) === -1){
+      var message = 'The CORS policy for this origin does not ' +
+                'allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //---------------------API SECTION---------------------\\
 
